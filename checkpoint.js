b69fdfb8d94272ae6ac6b5aa1b6c6d752ce1c78a
Gustavo-SB <gustavosb862@gmail.com>
2022-08-18 20:46:55 -0300

    fix if

--- checkpoint.js
+++ checkpoint.js
@@ -5,46 +5,55 @@
 // 4 - Feijão - 12s (padrão)
 // 5 - Brigadeiro - 8s (padrão)
 
-const comida = 1;
+const comida = 7;
 const tempo = 10;
 
 function microondas(comida, tempo) {
   let comidaSelecionada = "";
   let tempoPadrao = 0;
+  let podeFazer = false;
 
   if (comida == 1) {
     comidaSelecionada = "Pipoca";
     tempoPadrao = 10;
+    podeFazer = true;
   } else if (comida == 2) {
     comidaSelecionada = "Macarrão";
     tempoPadrao = 8;
+    podeFazer = true;
   } else if (comida == 3) {
     comidaSelecionada = "Carne";
     tempoPadrao = 15;
+    podeFazer = true;
   } else if (comida == 4) {
     comidaSelecionada = "Feijão";
     tempoPadrao = 12;
+    podeFazer = true;
   } else if (comida == 5) {
     comidaSelecionada = "Brigadeiro";
     tempoPadrao = 8;
+    podeFazer = true;
   } else {
     console.log("Prato inexistente");
+    podeFazer = false;
   }
 
-  console.log(`${comidaSelecionada} - ${tempo}s`);
+  if (podeFazer) {
+    console.log(`${comidaSelecionada} - ${tempo}s`);
 
-  for (let i = 0; i < tempo; i++) {
-    console.log(`${tempo - i} 🕒`);
-  }
+    for (let i = 0; i < tempo; i++) {
+      console.log(`${tempo - i} 🕒`);
+    }
 
-  console.log("Prato pronto, bom apetite!!! 🍽️");
+    console.log("Prato pronto, bom apetite!!! 🍽️");
 
-  if (tempo < tempoPadrao) {
-    return console.log("Tempo insuficiente ❄️")
-  } else if (tempo > (tempoPadrao * 2) && tempo <= (tempoPadrao * 3)) {
-    return console.log("A comida queimou 🔥")
-  } else if (tempo > (tempoPadrao * 3)) {
-    return console.log("Kabumm 💥")
+    if (tempo < tempoPadrao) {
+      return console.log("Tempo insuficiente ❄️")
+    } else if (tempo > (tempoPadrao * 2) && tempo <= (tempoPadrao * 3)) {
+      return console.log("A comida queimou 🔥")
+    } else if (tempo > (tempoPadrao * 3)) {
+      return console.log("Kabumm 💥")
+    }
   }
 }
 
