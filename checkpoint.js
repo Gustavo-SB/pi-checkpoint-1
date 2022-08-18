b3e0317629f7931c2063c8a947e57dd78fbd7c54
Gustavo-SB <gustavosb862@gmail.com>
2022-08-17 22:04:05 -0300

    emoti

--- checkpoint.js
+++ checkpoint.js
@@ -6,83 +6,83 @@
 // 5 - Brigadeiro - 8s (padrão)
 
 const comida = 1;
-const tempo = 10;
+const tempo = 30;
 
 function microondas(comida, tempo) {
   switch (comida) {
     case 1:
       const tempoPadraoPipoca = 10;
-      console.log(`Pipoca - ${tempo}s`);
+      console.log(`Pipoca - ${tempo}s 🕒`);
       for (let i = 0; i < tempo; i++) {
-        console.log(tempo - i);
+        console.log(`${tempo - i} 🕒`);
       }
-      console.log("Prato pronto, bom apetite!!!");
+      console.log("Prato pronto, bom apetite!!! 🍽️");
       if (tempo < tempoPadraoPipoca) {
-        return console.log("Tempo insuficiente")
+        return console.log("Tempo insuficiente ❄️")
       } else if (tempo >= (tempoPadraoPipoca * 2) && tempo < (tempoPadraoPipoca * 3)) {
-        return console.log("A comida queimou")
+        return console.log("A comida queimou 🔥")
       } else if (tempo >= (tempoPadraoPipoca * 3)) {
-        return console.log("Kabumm")
+        return console.log("Kabumm 💥")
       }
       break;
     case 2:
       const tempoPadraoMacarrao = 8;
-      console.log(`Macarrao - ${tempo}s`);
+      console.log(`Macarrao - ${tempo}s 🕒`);
       for (let i = 0; i < tempo; i++) {
-        console.log(tempo - i);
+        console.log(`${tempo - i} 🕒`);
       }
-      console.log("Prato pronto, bom apetite!!!");
+      console.log("Prato pronto, bom apetite!!! 🍽️");
       if (tempo < tempoPadraoMacarrao) {
-        return console.log("Tempo insuficiente")
+        return console.log("Tempo insuficiente ❄️")
       } else if (tempo >= (tempoPadraoMacarrao * 2) && tempo < (tempoPadraoMacarrao * 3)) {
-        return console.log("A comida queimou")
+        return console.log("A comida queimou 🔥")
       } else if (tempo >= (tempoPadraoMacarrao * 3)) {
-        return console.log("Kabumm")
+        return console.log("Kabumm 💥")
       }
       break;
     case 3:
       const tempoPadraoCarne = 15;
-      console.log(`Carne - ${tempo}s`);
+      console.log(`Carne - ${tempo}s 🕒`);
       for (let i = 0; i < tempo; i++) {
-        console.log(tempo - i);
+        console.log(`${tempo - i} 🕒`);
       }
-      console.log("Prato pronto, bom apetite!!!");
+      console.log("Prato pronto, bom apetite!!! 🍽️");
       if (tempo < tempoPadraoCarne) {
-        return console.log("Tempo insuficiente")
+        return console.log("Tempo insuficiente ❄️")
       } else if (tempo >= (tempoPadraoCarne * 2) && tempo < (tempoPadraoCarne * 3)) {
-        return console.log("A comida queimou")
+        return console.log("A comida queimou 🔥")
       } else if (tempo >= (tempoPadraoCarne * 3)) {
-        return console.log("Kabumm")
+        return console.log("Kabumm 💥")
       }
       break;
     case 4:
       const tempoPadraoFeijao = 12;
-      console.log(`Feijao - ${tempo}s`);
+      console.log(`Feijao - ${tempo}s 🕒`);
       for (let i = 0; i < tempo; i++) {
-        console.log(tempo - i);
+        console.log(`${tempo - i} 🕒`);
       }
-      console.log("Prato pronto, bom apetite!!!");
+      console.log("Prato pronto, bom apetite!!! 🍽️");
       if (tempo < tempoPadraoFeijao) {
-        return console.log("Tempo insuficiente")
+        return console.log("Tempo insuficiente ❄️")
       } else if (tempo >= (tempoPadraoFeijao * 2) && tempo < (tempoPadraoFeijao * 3)) {
-        return console.log("A comida queimou")
+        return console.log("A comida queimou 🔥")
       } else if (tempo >= (tempoPadraoFeijao * 3)) {
-        return console.log("Kabumm")
+        return console.log("Kabumm 💥")
       }
       break;
     case 5:
       const tempoPadraoBrigadeiro = 8;
-      console.log(`Brigadeiro - ${tempo}s`);
+      console.log(`Brigadeiro - ${tempo}s 🕒`);
       for (let i = 0; i < tempo; i++) {
-        console.log(tempo - i);
+        console.log(`${tempo - i} 🕒`);
       }
-      console.log("Prato pronto, bom apetite!!!");
+      console.log("Prato pronto, bom apetite!!! 🍽️");
       if (tempo < tempoPadraoBrigadeiro) {
-        return console.log("Tempo insuficiente")
+        return console.log("Tempo insuficiente ❄️")
       } else if (tempo >= (tempoPadraoBrigadeiro * 2) && tempo < (tempoPadraoBrigadeiro * 3)) {
-        return console.log("A comida queimou")
+        return console.log("A comida queimou 🔥")
       } else if (tempo >= (tempoPadraoBrigadeiro * 3)) {
-        return console.log("Kabumm")
+        return console.log("Kabumm 💥")
       }
       break;
     default:
